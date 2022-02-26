ccb88c4b90ad8c6a279b0eecbc98f657b044214b
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2022-02-26 18:49:59 +0000

    Push all code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert("Hello, welcome to today's web dev session")
+
+let celcius = prompt("Enter the temperature in celcius")
+
+let far = celcius * 1.8 + 32
+
+alert("Temperature in Fahrenheit is " + far)
+
+let name = prompt("What is your name?")
+
+alert("Welcome " + name + "!")
